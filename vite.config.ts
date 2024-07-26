838c37d4201b03a6db867a00c57f65f10ae5f8b9
deepesh <rddesign64@gmail.com>
2024-07-26 22:08:44 +0545

    vercel rewrites

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,25 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     visualizer({
+//       filename: './dist/stats.html', // The file where the visualization will be saved
+//       open: true, // Automatically open the visualization in the browser
+//     }),
+//   ],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id: string) {
+//           if (id.includes('node_modules')) {
+//             return id.toString().split('node_modules/')[1].split('/')[0].toString();
+//           }
+//         },
+//       },
+//     },
+//   },
+// });
